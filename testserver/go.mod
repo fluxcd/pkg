36b88cfb1d92b3fa41130b5a28ae8598d03ae207
Hidde Beydals <hello@hidde.co>
2020-08-18 11:30:58 +0200

    Move Git and Helm testservers to dedicated pkgs
    This is a better approach, because:
    
    1) The Helm server pulls in a lot of (indirect) dependencies from Helm,
       while those are unnecessary dependencies for most of the projects.
    2) The Git server has a replace statement in go.mod, given this needs
       to be copied over by any depending project, and most of the projects
       do not need to mock any git server, moving it to a dedicated pacakge
       clears them from this burden.

--- testserver/go.mod
+++ testserver/go.mod
@@ -1,14 +1,3 @@
 module github.com/fluxcd/pkg/testserver
 
 go 1.14
-
-// TODO(hidde): drop when PR is accepted:
-//  https://github.com/sosedoff/gitkit/pull/21
-replace github.com/sosedoff/gitkit => github.com/hiddeco/gitkit v0.2.1-0.20200422093229-4355fec70348
-
-require (
-	github.com/sosedoff/gitkit v0.2.0
-	helm.sh/helm/v3 v3.3.0
-	rsc.io/letsencrypt v0.0.3 // indirect
-	sigs.k8s.io/yaml v1.2.0
-)
--- testserver/http.go
+++ testserver/http.go
@@ -25,6 +25,8 @@ import (
 	"path/filepath"
 )
 
+// NewTempHTTPServer returns a HTTPServer with a newly created temp
+// dir as the docroot.
 func NewTempHTTPServer() (*HTTPServer, error) {
 	tmpDir, err := ioutil.TempDir("", "http-test-")
 	if err != nil {
@@ -34,6 +36,7 @@ func NewTempHTTPServer() (*HTTPServer, error) {
 	return srv, nil
 }
 
+// NewHTTPServer returns a HTTPServer with the given docroot set.
 func NewHTTPServer(docroot string) *HTTPServer {
 	root, err := filepath.Abs(docroot)
 	if err != nil {
@@ -44,17 +47,24 @@ func NewHTTPServer(docroot string) *HTTPServer {
 	}
 }
 
+// HTTPServer is an HTTP/S server for testing purposes.
+// It can serve files from the configured docroot and offers
+// a lightweight middleware configuration option.
 type HTTPServer struct {
 	docroot    string
 	middleware func(http.Handler) http.Handler
 	server     *httptest.Server
 }
 
+// WithMiddleware configures the middleware of the HTTPServer, this can for
+// example be used to configure Basic Auth credentials. It should be called
+// before starting the server, or requires a stop/start cycle.
 func (s *HTTPServer) WithMiddleware(m func(handler http.Handler) http.Handler) *HTTPServer {
 	s.middleware = m
 	return s
 }
 
+// Start starts the HTTPServer.
 func (s *HTTPServer) Start() {
 	s.server = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		handler := http.FileServer(http.Dir(s.docroot))
@@ -66,7 +76,8 @@ func (s *HTTPServer) Start() {
 	}))
 }
 
-func (s *HTTPServer) StartTLS(cert, key, ca []byte) error {
+// StartTLS starts the TLS HTTPServer with the given TLS configuration.
+func (s *HTTPServer) StartTLS(cert, key, ca []byte, serverName string) error {
 	s.server = httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		handler := http.FileServer(http.Dir(s.docroot))
 		if s.middleware != nil {
@@ -88,23 +99,27 @@ func (s *HTTPServer) StartTLS(cert, key, ca []byte) error {
 	cp.AppendCertsFromPEM(ca)
 	config.RootCAs = cp
 
-	config.ServerName = "example.com"
+	config.ServerName = serverName
 	s.server.TLS = &config
 
 	s.server.StartTLS()
 	return nil
 }
 
+// Stop stops the HTTPServer, if started.
 func (s *HTTPServer) Stop() {
 	if s.server != nil {
 		s.server.Close()
 	}
 }
 
+// Root returns the configured docroot of the HTTPServer.
 func (s *HTTPServer) Root() string {
 	return s.docroot
 }
 
+// URL returns the address the HTTPServer is listening at,
+// if started.
 func (s *HTTPServer) URL() string {
 	if s.server != nil {
 		return s.server.URL
