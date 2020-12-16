1a6324556a0806dbf494cfa90075fa89301173e7
Hidde Beydals <hello@hidde.co>
2020-12-16 22:06:39 +0100

    Turn Change into ReconcilateAtChanged predicate
    As predicates can be chained and used in conjunction with others,
    it is more useful to do just one thing in the predicate: detecting
    changes to the meta.ReconcileAtAnnotation.
    
    The generation changes can still be detected by chaining the predicate
    together with the predicate.GenerationChangedPredicate from
    controller-runtime itself using predicate.Or.
    
    Signed-off-by: Hidde Beydals <hello@hidde.co>

---
+++ runtime/predicates/reconcile_at_changed.go
@@ -0,0 +1,56 @@
+/*
+Copyright 2020 The Flux authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package predicates
+
+import (
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+
+	metav1 "github.com/fluxcd/pkg/apis/meta"
+)
+
+// ReconcilateAtChangedPredicate implements an update predicate
+// function for meta.ReconcileAtAnnotation changes.
+//
+// This predicate will skip update events that have no
+// meta.ReconcileAtAnnotation change.
+// It is intended to be used in conjunction with the
+// predicate.GenerationChangedPredicate, as in the following example:
+//
+// Controller.Watch(
+//		&source.Kind{Type: v1.MyCustomKind},
+// 		&handler.EnqueueRequestForObject{},
+//		predicate.Or(predicate.GenerationChangedPredicate{}, predicates.ReconcilateAtChangedPredicate{}))
+type ReconcilateAtChangedPredicate struct {
+	predicate.Funcs
+}
+
+// Update implements the default UpdateEvent filter for validating
+// meta.ReconcileAtAnnotation changes.
+func (ReconcilateAtChangedPredicate) Update(e event.UpdateEvent) bool {
+	if e.ObjectOld == nil || e.ObjectNew == nil {
+		return false
+	}
+
+	if val, ok := metav1.ReconcileAnnotationValue(e.ObjectOld.GetAnnotations()); ok {
+		if valOld, okOld := metav1.ReconcileAnnotationValue(e.ObjectNew.GetAnnotations()); okOld {
+			return val != valOld
+		}
+		return true
+	}
+	return false
+}
