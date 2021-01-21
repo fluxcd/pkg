05db12b44be0f550976fa1855e9fd359d0fc5dcb
Stefan Prodan <stefan.prodan@gmail.com>
2021-01-21 12:29:54 +0200

    Add Kubernetes client options (QPS and Burst) to runtime
    Signed-off-by: Stefan Prodan <stefan.prodan@gmail.com>

---
+++ runtime/client/client.go
@@ -0,0 +1,40 @@
+package client
+
+import (
+	"github.com/spf13/pflag"
+	"k8s.io/client-go/rest"
+	ctrl "sigs.k8s.io/controller-runtime"
+)
+
+const (
+	flagQPS   = "kube-api-qps"
+	flagBurst = "kube-api-burst"
+)
+
+// Options contains the configuration options for the Kubernetes client.
+type Options struct {
+	// QPS indicates the maximum queries-per-second of
+	//requests sent to to the Kubernetes API, defaults to 20.
+	QPS float32
+
+	// Burst indicates the maximum burst queries-per-second of
+	// requests sent to the Kubernetes API, defaults to 50.
+	Burst int
+}
+
+// BindFlags will parse the given flagset for Kubernetes client option flags and
+// set the Options accordingly.
+func (o *Options) BindFlags(fs *pflag.FlagSet) {
+	fs.Float32Var(&o.QPS, flagQPS, 20.0,
+		"The maximum queries-per-second of requests sent to the Kubernetes API.")
+	fs.IntVar(&o.Burst, flagBurst, 50,
+		"The maximum burst queries-per-second of requests sent to the Kubernetes API.")
+}
+
+// GetConfigOrDie wraps ctrl.GetConfigOrDie and sets the QPS and Bust options
+func GetConfigOrDie(opts Options) *rest.Config {
+	restConfig := ctrl.GetConfigOrDie()
+	restConfig.QPS = opts.QPS
+	restConfig.Burst = opts.Burst
+	return restConfig
+}
