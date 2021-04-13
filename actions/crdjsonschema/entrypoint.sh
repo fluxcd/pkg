49e26aa2ee9e734c3233c560253fd9542afe18ae
Stefan Prodan <stefan.prodan@gmail.com>
2021-04-13 11:40:24 +0300

    Split multi-doc CRDs
    Signed-off-by: Stefan Prodan <stefan.prodan@gmail.com>

--- actions/crdjsonschema/entrypoint.sh
+++ actions/crdjsonschema/entrypoint.sh
@@ -4,11 +4,19 @@ set -eu
 
 CRD_PATH=${1}
 OUTPUT_DIR=${2-master-standalone}
-
-JSON=$(python /openapi2jsonschema.py ${CRD_PATH})
-
 mkdir -p ${OUTPUT_DIR}
 
-mv ${JSON} ${OUTPUT_DIR}
+/kubernetes-split-yaml $CRD_PATH
 
-echo "OpenAPI JSON schema saved to ${OUTPUT_DIR}/${JSON}"
+FILES=generated/*
+for f in $FILES
+do
+  JSON=$(python /openapi2jsonschema.py ${f})
+  mv ${JSON} ${OUTPUT_DIR}
+done
+
+echo "OpenAPI JSON schemas saved to ${OUTPUT_DIR}"
+for f in ${OUTPUT_DIR}/*
+do
+  cat $f
+done
