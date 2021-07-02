94067b9f926b322b3dec7d4999236238b9758184
Hidde Beydals <hello@hidde.co>
2021-07-02 17:07:52 +0200

    Document runtime client package
    Signed-off-by: Hidde Beydals <hello@hidde.co>

--- runtime/client/client.go
+++ runtime/client/client.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2021 The Flux authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package client
 
 import (
@@ -11,19 +27,33 @@ const (
 	flagBurst = "kube-api-burst"
 )
 
-// Options contains the configuration options for the Kubernetes client.
+// Options contains the runtime configuration for a Kubernetes client.
+//
+// The struct can be used in the main.go file of your controller by binding it to the main flag set, and then utilizing
+// the configured options later:
+//
+//	func main() {
+//		var (
+//			// other controller specific configuration variables
+//			clientOptions client.Options
+//		)
+//
+//		// Bind the options to the main flag set, and parse it
+//		clientOptions.BindFlags(flag.CommandLine)
+//		flag.Parse()
+//
+//		// Get a runtime Kubernetes client configuration with the options set
+//		restConfig := client.GetConfigOrDie(clientOptions)
+//  }
 type Options struct {
-	// QPS indicates the maximum queries-per-second of
-	//requests sent to to the Kubernetes API, defaults to 20.
+	// QPS indicates the maximum queries-per-second of requests sent to to the Kubernetes API, defaults to 20.
 	QPS float32
 
-	// Burst indicates the maximum burst queries-per-second of
-	// requests sent to the Kubernetes API, defaults to 50.
+	// Burst indicates the maximum burst queries-per-second of requests sent to the Kubernetes API, defaults to 50.
 	Burst int
 }
 
-// BindFlags will parse the given flagset for Kubernetes client option flags and
-// set the Options accordingly.
+// BindFlags will parse the given pflag.FlagSet for Kubernetes client option flags and set the Options accordingly.
 func (o *Options) BindFlags(fs *pflag.FlagSet) {
 	fs.Float32Var(&o.QPS, flagQPS, 20.0,
 		"The maximum queries-per-second of requests sent to the Kubernetes API.")
@@ -31,10 +61,10 @@ func (o *Options) BindFlags(fs *pflag.FlagSet) {
 		"The maximum burst queries-per-second of requests sent to the Kubernetes API.")
 }
 
-// GetConfigOrDie wraps ctrl.GetConfigOrDie and sets the QPS and Bust options
+// GetConfigOrDie wraps ctrl.GetConfigOrDie and sets the configured Options, returning the modified rest.Config.
 func GetConfigOrDie(opts Options) *rest.Config {
-	restConfig := ctrl.GetConfigOrDie()
-	restConfig.QPS = opts.QPS
-	restConfig.Burst = opts.Burst
-	return restConfig
+	config := ctrl.GetConfigOrDie()
+	config.QPS = opts.QPS
+	config.Burst = opts.Burst
+	return config
 }
