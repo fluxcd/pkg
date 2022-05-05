4f6f288de676454f7a2a7bc9dc89c1335af7a80b
Paulo Gomes <paulo.gomes@weave.works>
2022-05-05 09:53:13 +0100

    ssh: Add GenerateKeyPair For unit tests that need to generate different types of keypairs, having a type based generator makes the code implementation much simpler and avoid code duplication.
    Signed-off-by: Paulo Gomes <paulo.gomes@weave.works>

--- ssh/key_pair.go
+++ ssh/key_pair.go
@@ -24,6 +24,7 @@ import (
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
+	"fmt"
 
 	"golang.org/x/crypto/ssh"
 )
@@ -36,6 +37,39 @@ type KeyPair struct {
 
 type KeyPairGenerator interface {
 	Generate() (*KeyPair, error)
+}
+
+type KeyPairType string
+
+const (
+	// RSA_4096 represents a RSA keypair with 4096 bits.
+	RSA_4096 KeyPairType = "rsa-4096"
+	// ECDSA_P256 represents a ecdsa keypair using Curve P-256.
+	ECDSA_P256 KeyPairType = "ecdsa-p256"
+	// ECDSA_P384 represents a ecdsa keypair using Curve P-384.
+	ECDSA_P384 KeyPairType = "ecdsa-p384"
+	// ECDSA_P521 represents a ecdsa keypair using Curve P-521.
+	ECDSA_P521 KeyPairType = "ecdsa-p521"
+	// ED25519 represents a ed25519 keypair.
+	ED25519 KeyPairType = "ed25519"
+)
+
+// GenerateKeyPair generates a keypair based on KeyPairType.
+func GenerateKeyPair(keyType KeyPairType) (*KeyPair, error) {
+	switch keyType {
+	case RSA_4096:
+		return NewRSAGenerator(4096).Generate()
+	case ECDSA_P256:
+		return NewECDSAGenerator(elliptic.P256()).Generate()
+	case ECDSA_P384:
+		return NewECDSAGenerator(elliptic.P384()).Generate()
+	case ECDSA_P521:
+		return NewECDSAGenerator(elliptic.P521()).Generate()
+	case ED25519:
+		return NewEd25519Generator().Generate()
+	default:
+		return nil, fmt.Errorf("unsupported key type: %s", keyType)
+	}
 }
 
 type RSAGenerator struct {
