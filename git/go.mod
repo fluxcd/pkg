1e8d9ba55997d7db4a3073e02180c9065d66c26f
Sanskar Jaiswal <jaiswalsanskar078@gmail.com>
2022-06-23 21:24:07 +0530

    add new package: git
    Adds a new package named `git`, which defines a common API for all git
    clients, along with various utilities and helper methods.
    
    Signed-off-by: Sanskar Jaiswal <jaiswalsanskar078@gmail.com>

---
+++ git/go.mod
@@ -0,0 +1,51 @@
+module github.com/fluxcd/pkg/git
+
+go 1.17
+
+require (
+	github.com/Masterminds/semver/v3 v3.1.1
+	github.com/ProtonMail/go-crypto v0.0.0-20220517143526-88bb52951d5b
+	github.com/fluxcd/gitkit v0.5.1
+	github.com/fluxcd/pkg/gittestserver v0.5.4
+	github.com/fluxcd/pkg/gitutil v0.1.0
+	github.com/fluxcd/pkg/ssh v0.5.0
+	github.com/fluxcd/pkg/version v0.1.0
+	github.com/go-git/go-billy/v5 v5.3.1
+	github.com/go-git/go-git/v5 v5.4.2
+	github.com/onsi/gomega v1.19.0
+	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e
+	k8s.io/api v0.24.1
+)
+
+require (
+	github.com/Microsoft/go-winio v0.5.2 // indirect
+	github.com/acomagu/bufpipe v1.0.3 // indirect
+	github.com/emirpasic/gods v1.18.1 // indirect
+	github.com/go-git/gcfg v1.5.0 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/gofrs/uuid v4.2.0+incompatible // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/google/go-cmp v0.5.8 // indirect
+	github.com/google/gofuzz v1.2.0 // indirect
+	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/kevinburke/ssh_config v1.2.0 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/sergi/go-diff v1.2.0 // indirect
+	github.com/xanzy/ssh-agent v0.3.1 // indirect
+	golang.org/x/net v0.0.0-20220607020251-c690dde0001d // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/warnings.v0 v0.1.2 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	k8s.io/apimachinery v0.24.1 // indirect
+	k8s.io/klog/v2 v2.60.1 // indirect
+	k8s.io/utils v0.0.0-20220210201930-3a6ce19ff2f9 // indirect
+	sigs.k8s.io/json v0.0.0-20211208200746-9f7c6b3444d2 // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.2.1 // indirect
+	sigs.k8s.io/yaml v1.3.0 // indirect
+)
---
+++ git/options.go
@@ -0,0 +1,225 @@
+/*
+Copyright 2021 The Flux authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package git
+
+import (
+	"fmt"
+	"net/url"
+
+	v1 "k8s.io/api/core/v1"
+)
+
+const (
+	DefaultRemote            = "origin"
+	DefaultBranch            = "master"
+	DefaultPublicKeyAuthUser = "git"
+)
+
+// CheckoutOptions are the options used for a Git checkout.
+type CheckoutOptions struct {
+	// Branch to checkout, can be combined with Branch with some
+	// Implementations.
+	Branch string
+
+	// Tag to checkout, takes precedence over Branch.
+	Tag string
+
+	// SemVer tag expression to checkout, takes precedence over Tag.
+	SemVer string `json:"semver,omitempty"`
+
+	// Commit SHA1 to checkout, takes precedence over Tag and SemVer,
+	// can be combined with Branch with some Implementations.
+	Commit string
+
+	// RecurseSubmodules defines if submodules should be checked out,
+	// not supported by all Implementations.
+	RecurseSubmodules bool
+
+	// LastRevision holds the revision observed on the last successful
+	// reconciliation.
+	// It is used to skip clone operations when no changes were detected.
+	LastRevision string
+
+	// ShallowClone defines if the repository should be shallow cloned,
+	// not supported by all implementations
+	ShallowClone bool
+}
+
+type TransportType string
+
+const (
+	SSH   TransportType = "ssh"
+	HTTPS TransportType = "https"
+	HTTP  TransportType = "http"
+)
+
+// AuthOptions are the authentication options for the Transport of
+// communication with a remote origin.
+type AuthOptions struct {
+	Transport  TransportType
+	Host       string
+	Username   string
+	Password   string
+	Identity   []byte
+	KnownHosts []byte
+	CAFile     []byte
+}
+
+// KexAlgos hosts the key exchange algorithms to be used for SSH connections.
+// If empty, Go's default is used instead.
+var KexAlgos []string
+
+// HostKeyAlgos holds the HostKey algorithms that the SSH client will advertise
+// to the server. If empty, Go's default is used instead.
+var HostKeyAlgos []string
+
+// Validate the AuthOptions against the defined Transport.
+func (o AuthOptions) Validate() error {
+	switch o.Transport {
+	case HTTPS, HTTP:
+		if o.Username == "" && o.Password != "" {
+			return fmt.Errorf("invalid '%s' auth option: 'password' requires 'username' to be set", o.Transport)
+		}
+	case SSH:
+		if o.Host == "" {
+			return fmt.Errorf("invalid '%s' auth option: 'host' is required", o.Transport)
+		}
+		if len(o.Identity) == 0 {
+			return fmt.Errorf("invalid '%s' auth option: 'identity' is required", o.Transport)
+		}
+		if len(o.KnownHosts) == 0 {
+			return fmt.Errorf("invalid '%s' auth option: 'known_hosts' is required", o.Transport)
+		}
+	case "":
+		return fmt.Errorf("no transport type set")
+	default:
+		return fmt.Errorf("unknown transport '%s'", o.Transport)
+	}
+	return nil
+}
+
+// AuthOptionsFromSecret constructs an AuthOptions object from the given Secret,
+// and then validates the result. It returns the AuthOptions, or an error.
+func AuthOptionsFromMap(URL string, data map[string][]byte) (*AuthOptions, error) {
+	if len(data) == 0 {
+		return nil, fmt.Errorf("no secret provided to construct auth strategy from")
+	}
+
+	u, err := url.Parse(URL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse URL to determine auth strategy: %w", err)
+	}
+
+	opts := &AuthOptions{
+		Transport:  TransportType(u.Scheme),
+		Host:       u.Host,
+		Username:   string(data["username"]),
+		Password:   string(data["password"]),
+		CAFile:     data["caFile"],
+		Identity:   data["identity"],
+		KnownHosts: data["known_hosts"],
+	}
+	if opts.Username == "" {
+		opts.Username = u.User.Username()
+	}
+	if opts.Username == "" {
+		opts.Username = DefaultPublicKeyAuthUser
+	}
+
+	if err = opts.Validate(); err != nil {
+		return nil, err
+	}
+
+	return opts, nil
+}
+
+// NewAuthOptions constructs a minimal AuthOptions object from the
+// given URL and then validates the result. It returns the AuthOptions, or an
+// error.
+func NewAuthOptions(URL string) (*AuthOptions, error) {
+	u, err := url.Parse(URL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse URL to determine auth strategy: %w", err)
+	}
+
+	opts := &AuthOptions{
+		Transport: TransportType(u.Scheme),
+		Host:      u.Host,
+	}
+
+	if err = opts.Validate(); err != nil {
+		return nil, err
+	}
+
+	return opts, nil
+}
+
+// AuthOptionsFromSecret constructs an AuthOptions object from the given Secret,
+// and then validates the result. It returns the AuthOptions, or an error.
+func AuthOptionsFromSecret(URL string, secret *v1.Secret) (*AuthOptions, error) {
+	if secret == nil {
+		return nil, fmt.Errorf("no secret provided to construct auth strategy from")
+	}
+
+	u, err := url.Parse(URL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse URL to determine auth strategy: %w", err)
+	}
+
+	opts := &AuthOptions{
+		Transport:  TransportType(u.Scheme),
+		Host:       u.Host,
+		Username:   string(secret.Data["username"]),
+		Password:   string(secret.Data["password"]),
+		CAFile:     secret.Data["caFile"],
+		Identity:   secret.Data["identity"],
+		KnownHosts: secret.Data["known_hosts"],
+	}
+	if opts.Username == "" {
+		opts.Username = u.User.Username()
+	}
+	if opts.Username == "" {
+		opts.Username = DefaultPublicKeyAuthUser
+	}
+
+	if err = opts.Validate(); err != nil {
+		return nil, err
+	}
+
+	return opts, nil
+}
+
+// AuthOptionsWithoutSecret constructs a minimal AuthOptions object from the
+// given URL and then validates the result. It returns the AuthOptions, or an
+// error.
+func AuthOptionsWithoutSecret(URL string) (*AuthOptions, error) {
+	u, err := url.Parse(URL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse URL to determine auth strategy: %w", err)
+	}
+
+	opts := &AuthOptions{
+		Transport: TransportType(u.Scheme),
+		Host:      u.Host,
+	}
+
+	if err = opts.Validate(); err != nil {
+		return nil, err
+	}
+
+	return opts, nil
+}
