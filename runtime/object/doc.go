f11f646fb1d59891f0e5a04b970373ce2358b475
Sunny <darkowlzz@protonmail.com>
2022-08-25 02:43:41 +0530

    Add runtime/object pkg for runtime.Object helpers
    runtime/object package provides helpers for querying and mutating the
    attributes of a runtime.Object without knowing their static types
    using unstructured objects.
    
    Signed-off-by: Sunny <darkowlzz@protonmail.com>

---
+++ runtime/object/doc.go
@@ -0,0 +1,20 @@
+/*
+Copyright 2022 The Flux authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package object provides helpers for interacting with GitOps Toolkit objects
+// using unstructured types. The helpers assist in reading and writing certain
+// attributes of the objects without converting them to their original types.
+package object
