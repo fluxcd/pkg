2b9553c975d53392b4b750ae458039eb8d33ea20
Sunny <darkowlzz@protonmail.com>
2023-01-12 12:58:41 +0000

    gitlab git e2e test improvements
    - cleanup gitlab container even on test failure
    - rename input argument variable to GO_TEST_ARGS as it's go test
      arguments
    - simplify the gitlab test matcher which also allows running specific
      subtests for gitlab tests now
    - provide option to persist the gitlab container and reuse in subsequent
      test execution
    - generate random gitlab PAT per test execution, allowing subsequent
      test execution to not conflict
    - no sleep when gitlab is found to be healthy
    - support for specifying registry proxy for the gitlab container image
    - rename gitlab container to be unique to flux tests
    - retry grabbing root password by restarting gitlab
    
    Signed-off-by: Sunny <darkowlzz@protonmail.com>

--- git/internal/e2e/run.sh
+++ git/internal/e2e/run.sh
@@ -1,23 +1,25 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 # This script runs e2e tests for pkg/git/gogit and pkg/git/libgit2.
 
 set -o errexit
+
 PROJECT_DIR=$(git rev-parse --show-toplevel)
 DIR="$(cd "$(dirname "$0")" && pwd)"
+GITLAB_CONTAINER=gitlab-flux-e2e
 
 cd "${PROJECT_DIR}/git/libgit2" && make libgit2
 LIBGIT2_BUILD_DIR=${PROJECT_DIR}/git/libgit2/build . "${PROJECT_DIR}/git/libgit2/libgit2-vars.env"
 
-if [[ "${GO_TESTS}" = "" ]] || [[ "${GO_TESTS}" = "-run TestGitLabCEE2E" ]]; then
+if [[ "${GO_TEST_ARGS}" = "" ]] || [[ "${GO_TEST_ARGS}" = *"TestGitLabCEE2E"* ]]; then
+    # Cleanup gitlab container if persistence is not enabled.
+    if [[ -z "${PERSIST_GITLAB}" ]]; then
+        trap "docker kill ${GITLAB_CONTAINER} && docker rm ${GITLAB_CONTAINER}" EXIT
+    fi
     source "${DIR}/setup_gitlab.sh"
 fi
 
 cd "${DIR}"
 CGO_LDFLAGS=$(PKG_CONFIG_PATH="${PKG_CONFIG_PATH}" pkg-config --libs --static --cflags libgit2)
 PKG_CONFIG_PATH="${PKG_CONFIG_PATH}" CGO_LDFLAGS="${CGO_LDFLAGS}" CGO_ENABLED=1 \
-    go test -v -tags 'netgo,osusergo,static_build,e2e' -race ${GO_TESTS} ./...
-
-# cleanup
-if [[ "${GO_TESTS}" = "" ]] || [[ "${GO_TESTS}" = "-run TestGitLabCEE2E" ]]; then
-    docker kill gitlab && docker rm gitlab
-fi
+    go test -v -tags 'netgo,osusergo,static_build,e2e' -race ${GO_TEST_ARGS} ./...
