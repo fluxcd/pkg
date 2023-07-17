30fd85d9700985c1bda926db409d9aceee9040db
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2023-07-17 22:58:17 +0100

    add failfast option
    Signed-off-by: Somtochi Onyekwere <somtochionyekwere@gmail.com>

--- ssa/manager_wait_test.go
+++ ssa/manager_wait_test.go
@@ -19,11 +19,17 @@ package ssa
 
 import (
 	"context"
+	"fmt"
+	"strings"
 	"testing"
 	"time"
 
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"sigs.k8s.io/cli-utils/pkg/kstatus/status"
 	"sigs.k8s.io/cli-utils/pkg/object"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -59,7 +65,7 @@ func TestWaitForSet(t *testing.T) {
 			t.Fatal(err)
 		}
 
-		if err := manager.WaitForSet(changeSet.ToObjMetadataSet(), WaitOptions{time.Second, 3 * time.Second}); err == nil {
+		if err := manager.WaitForSet(changeSet.ToObjMetadataSet(), WaitOptions{time.Second, 3 * time.Second, false}); err == nil {
 			t.Error("wanted wait error due to observedGeneration < generation")
 		}
 
@@ -92,6 +98,166 @@ func TestWaitForSet(t *testing.T) {
 
 		if err := manager.WaitForSet(changeSet.ToObjMetadataSet(), DefaultWaitOptions()); err != nil {
 			t.Errorf("wait error: %v", err)
+		}
+	})
+}
+
+func TestWaitForSet_failFast(t *testing.T) {
+	timeout := 5 * time.Second
+	interval := 2 * time.Second
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+
+	id := generateName("failfast")
+	objects, err := readManifest("testdata/test10.yaml", id)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	manager.SetOwnerLabels(objects, "infra", "default")
+	_, pvc := getFirstObject(objects, "PersistentVolumeClaim", id)
+	_, deploy := getFirstObject(objects, "Deployment", id)
+
+	deployObjMeta, _ := object.RuntimeToObjMeta(deploy)
+
+	cs, err := manager.ApplyAllStaged(ctx, objects, DefaultApplyOptions())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var clusterDeploy = &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      id,
+			Namespace: id,
+		},
+	}
+	err = manager.client.Get(ctx, client.ObjectKeyFromObject(deploy), clusterDeploy)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Set Progressing Condition to false and reason to ProgressDeadlineExceeded
+	// This tells kstatus that the deployment has stalled.
+	cond := appsv1.DeploymentCondition{
+		Type:               appsv1.DeploymentProgressing,
+		Status:             corev1.ConditionFalse,
+		LastTransitionTime: metav1.Time{},
+		Reason:             "ProgressDeadlineExceeded",
+		Message:            "timeout progressing",
+	}
+	clusterDeploy.Status = appsv1.DeploymentStatus{
+		ObservedGeneration:  clusterDeploy.Generation,
+		Replicas:            *clusterDeploy.Spec.Replicas,
+		UpdatedReplicas:     *clusterDeploy.Spec.Replicas,
+		UnavailableReplicas: *clusterDeploy.Spec.Replicas,
+		Conditions:          []appsv1.DeploymentCondition{cond},
+	}
+	err = manager.client.Status().Update(ctx, clusterDeploy)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	clusterPvc := &unstructured.Unstructured{}
+	clusterPvc.SetGroupVersionKind(schema.GroupVersionKind{
+		Group:   "",
+		Kind:    "PersistentVolumeClaim",
+		Version: "v1",
+	})
+	if err := manager.client.Get(ctx, client.ObjectKeyFromObject(pvc), clusterPvc); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := unstructured.SetNestedField(clusterPvc.Object, "Bound", "status", "phase"); err != nil {
+		t.Fatal(err)
+	}
+
+	opts := &client.SubResourcePatchOptions{
+		PatchOptions: client.PatchOptions{
+			FieldManager: manager.owner.Field,
+		},
+	}
+
+	clusterPvc.SetManagedFields(nil)
+	if err := manager.client.Status().Patch(ctx, clusterPvc, client.Apply, opts); err != nil {
+		t.Fatal(err)
+	}
+
+	t.Run("timeout when failfast is set to false", func(t *testing.T) {
+		err = manager.WaitForSet(cs.ToObjMetadataSet(), WaitOptions{
+			Interval: interval,
+			Timeout:  timeout,
+			FailFast: false,
+		})
+
+		deployFailedMsg := fmt.Sprintf("%s status: '%s'", FmtObjMetadata(deployObjMeta), status.FailedStatus)
+
+		if err == nil || !strings.Contains(err.Error(), "timeout waiting for") {
+			t.Fatal("expected WaitForSet to timeout waiting for deployment")
+		}
+
+		if !strings.Contains(err.Error(), deployFailedMsg) {
+			t.Fatal("expected error to contain status of failed deployment")
+		}
+	})
+
+	t.Run("return early when failfast is set to true", func(t *testing.T) {
+		err = manager.WaitForSet(cs.ToObjMetadataSet(), WaitOptions{
+			Interval: interval,
+			Timeout:  timeout,
+			FailFast: true,
+		})
+
+		deployFailedMsg := fmt.Sprintf("%s status: '%s'", FmtObjMetadata(deployObjMeta), status.FailedStatus)
+
+		if err == nil || !strings.Contains(err.Error(), "failed early") {
+			t.Fatal("expected WaitForSet to fail early due to stalled deployment")
+		}
+
+		if !strings.Contains(err.Error(), deployFailedMsg) {
+			t.Fatal("expected error to contain status of failed deployment")
+		}
+	})
+
+	t.Run("fail early even if there are still Progressing resources", func(t *testing.T) {
+		// change status to Pending to have an 'InProgress' resource
+		clusterPvc := &unstructured.Unstructured{}
+		clusterPvc.SetGroupVersionKind(schema.GroupVersionKind{
+			Group:   "",
+			Kind:    "PersistentVolumeClaim",
+			Version: "v1",
+		})
+		if err := manager.client.Get(ctx, client.ObjectKeyFromObject(pvc), clusterPvc); err != nil {
+			t.Fatal(err)
+		}
+
+		if err := unstructured.SetNestedField(clusterPvc.Object, "Pending", "status", "phase"); err != nil {
+			t.Fatal(err)
+		}
+		opts := &client.SubResourcePatchOptions{
+			PatchOptions: client.PatchOptions{
+				FieldManager: manager.owner.Field,
+			},
+		}
+
+		clusterPvc.SetManagedFields(nil)
+		if err := manager.client.Status().Patch(ctx, clusterPvc, client.Apply, opts); err != nil {
+			t.Fatal(err)
+		}
+
+		err = manager.WaitForSet(cs.ToObjMetadataSet(), WaitOptions{
+			Interval: interval,
+			Timeout:  timeout,
+			FailFast: true,
+		})
+
+		deployFailedMsg := fmt.Sprintf("%s status: '%s'", FmtObjMetadata(deployObjMeta), status.FailedStatus)
+
+		if err == nil || !strings.Contains(err.Error(), "failed early") {
+			t.Fatal("expected WaitForSet to fail early due to stalled deployment")
+		}
+
+		if !strings.Contains(err.Error(), deployFailedMsg) {
+			t.Fatal("expected error to contain status of failed deployment")
 		}
 	})
 }
