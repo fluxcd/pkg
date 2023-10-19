92953af24194e0e63cd4c68867de5ce279f09e66
Hidde Beydals <hidde@hhh.computer>
2023-10-19 15:15:00 +0200

    ssa/jsondiff: add more tests around selectors
    Signed-off-by: Hidde Beydals <hidde@hhh.computer>

--- ssa/jsondiff/selector_test.go
+++ ssa/jsondiff/selector_test.go
@@ -197,3 +197,355 @@ func TestSelectorRegex_MatchUnstructured(t *testing.T) {
 		})
 	}
 }
+
+func TestSelectorRegex_MatchGVK(t *testing.T) {
+	tests := []struct {
+		name     string
+		selector *Selector
+		group    string
+		version  string
+		kind     string
+		want     bool
+	}{
+		{
+			name: "valid match",
+			selector: &Selector{
+				Group:   "apps",
+				Version: "v1",
+				Kind:    "Deployment",
+			},
+			group:   "apps",
+			version: "v1",
+			kind:    "Deployment",
+			want:    true,
+		},
+		{
+			name: "valid match with regex",
+			selector: &Selector{
+				Group:   "apps",
+				Version: "v.*",
+				Kind:    "Deployment",
+			},
+			group:   "apps",
+			version: "v1",
+			kind:    "Deployment",
+			want:    true,
+		},
+		{
+			name:     "valid match without regex",
+			selector: &Selector{},
+			group:    "apps",
+			version:  "v1",
+			kind:     "Deployment",
+			want:     true,
+		},
+		{
+			name: "invalid group",
+			selector: &Selector{
+				Group:   "apps",
+				Version: "v1",
+				Kind:    "Deployment",
+			},
+			group:   "extensions",
+			version: "v1",
+			kind:    "Deployment",
+			want:    false,
+		},
+		{
+			name: "invalid version",
+			selector: &Selector{
+				Group:   "apps",
+				Version: "v1",
+				Kind:    "Deployment",
+			},
+			group:   "apps",
+			version: "v2",
+			kind:    "Deployment",
+			want:    false,
+		},
+		{
+			name: "invalid kind",
+			selector: &Selector{
+				Group:   "apps",
+				Version: "v1",
+				Kind:    "Deployment",
+			},
+			group:   "apps",
+			version: "v1",
+			kind:    "StatefulSet",
+			want:    false,
+		},
+		{
+			name: "partial selector",
+			selector: &Selector{
+				Group: "apps",
+				Kind:  "Deployment",
+			},
+			group:   "apps",
+			version: "v2",
+			kind:    "Deployment",
+			want:    true,
+		},
+		{
+			name:     "empty selector",
+			selector: &Selector{},
+			want:     true,
+		},
+		{
+			name:     "nil selector",
+			selector: nil,
+			want:     true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s, err := NewSelectorRegex(tt.selector)
+			if err != nil {
+				t.Errorf("NewSelectorRegex() error = %v", err)
+				return
+			}
+
+			if got := s.MatchGVK(tt.group, tt.version, tt.kind); got != tt.want {
+				t.Errorf("MatchGVK() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSelectorRegex_MatchName(t *testing.T) {
+	tests := []struct {
+		name     string
+		selector *Selector
+		n        string
+		want     bool
+	}{
+		{
+			name: "valid match",
+			selector: &Selector{
+				Name: "name-1",
+			},
+			n:    "name-1",
+			want: true,
+		},
+		{
+			name: "valid match with regex",
+			selector: &Selector{
+				Name: "name-.*",
+			},
+			n:    "name-2",
+			want: true,
+		},
+		{
+			name: "invalid name",
+			selector: &Selector{
+				Name: "name-.*",
+			},
+			n:    "other-name-1",
+			want: false,
+		},
+		{
+			name:     "empty selector",
+			selector: &Selector{},
+			n:        "any-name",
+			want:     true,
+		},
+		{
+			name:     "nil selector",
+			selector: nil,
+			n:        "any-name",
+			want:     true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s, err := NewSelectorRegex(tt.selector)
+			if err != nil {
+				t.Errorf("NewSelectorRegex() error = %v", err)
+				return
+			}
+
+			if got := s.MatchName(tt.n); got != tt.want {
+				t.Errorf("MatchName() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSelectorRegex_MatchNamespace(t *testing.T) {
+	tests := []struct {
+		name     string
+		selector *Selector
+		ns       string
+		want     bool
+	}{
+		{
+			name: "valid match",
+			selector: &Selector{
+				Namespace: "ns-1",
+			},
+			ns:   "ns-1",
+			want: true,
+		},
+		{
+			name: "valid match with regex",
+			selector: &Selector{
+				Namespace: "ns-.*",
+			},
+			ns:   "ns-2",
+			want: true,
+		},
+		{
+			name: "invalid namespace",
+			selector: &Selector{
+				Namespace: "ns-.*",
+			},
+			ns:   "other-ns-1",
+			want: false,
+		},
+		{
+			name:     "empty selector",
+			selector: &Selector{},
+			ns:       "any-ns",
+			want:     true,
+		},
+		{
+			name:     "nil selector",
+			selector: nil,
+			ns:       "any-ns",
+			want:     true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s, err := NewSelectorRegex(tt.selector)
+			if err != nil {
+				t.Errorf("NewSelectorRegex() error = %v", err)
+				return
+			}
+
+			if got := s.MatchNamespace(tt.ns); got != tt.want {
+				t.Errorf("MatchNamespace() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSelectorRegex_MatchAnnotationSelector(t *testing.T) {
+	tests := []struct {
+		name        string
+		selector    *Selector
+		annotations map[string]string
+		want        bool
+	}{
+		{
+			name: "valid match",
+			selector: &Selector{
+				AnnotationSelector: "key-1=value-1",
+			},
+			annotations: map[string]string{
+				"key-1": "value-1",
+				"key-2": "value-2",
+			},
+			want: true,
+		},
+		{
+			name: "invalid annotation",
+			selector: &Selector{
+				AnnotationSelector: "key-1 in(value-1)",
+			},
+			annotations: map[string]string{
+				"key-1": "value-2",
+			},
+			want: false,
+		},
+		{
+			name:     "empty selector",
+			selector: &Selector{},
+			annotations: map[string]string{
+				"key-1": "value-1",
+			},
+			want: true,
+		},
+		{
+			name:     "nil selector",
+			selector: nil,
+			annotations: map[string]string{
+				"key-2": "value-2",
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s, err := NewSelectorRegex(tt.selector)
+			if err != nil {
+				t.Errorf("NewSelectorRegex() error = %v", err)
+				return
+			}
+
+			if got := s.MatchAnnotationSelector(tt.annotations); got != tt.want {
+				t.Errorf("MatchAnnotationSelector() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSelectorRegex_MatchLabelSelector(t *testing.T) {
+	tests := []struct {
+		name     string
+		selector *Selector
+		labels   map[string]string
+		want     bool
+	}{
+		{
+			name: "valid match",
+			selector: &Selector{
+				LabelSelector: "key-1=value-1",
+			},
+			labels: map[string]string{
+				"key-1": "value-1",
+				"key-2": "value-2",
+			},
+			want: true,
+		},
+		{
+			name: "invalid label",
+			selector: &Selector{
+				LabelSelector: "key-1 in(value-1)",
+			},
+			labels: map[string]string{
+				"key-1": "value-2",
+			},
+			want: false,
+		},
+		{
+			name:     "empty selector",
+			selector: &Selector{},
+			labels: map[string]string{
+				"key-1": "value-1",
+			},
+			want: true,
+		},
+		{
+			name:     "nil selector",
+			selector: nil,
+			labels: map[string]string{
+				"key-2": "value-2",
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s, err := NewSelectorRegex(tt.selector)
+			if err != nil {
+				t.Errorf("NewSelectorRegex() error = %v", err)
+				return
+			}
+
+			if got := s.MatchLabelSelector(tt.labels); got != tt.want {
+				t.Errorf("MatchLabelSelector() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
