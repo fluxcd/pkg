09ba5d81eff43c5d236460eb7e269700fff06a41
Stefan Prodan <stefan.prodan@gmail.com>
2023-11-20 13:06:42 +0200

    runtime: Add `pprof.GetHandlers` to help setup the metrics server
    Signed-off-by: Stefan Prodan <stefan.prodan@gmail.com>

--- runtime/pprof/pprof.go
+++ runtime/pprof/pprof.go
@@ -19,6 +19,7 @@ package pprof
 import (
 	"net/http"
 	"net/http/pprof"
+	"runtime"
 )
 
 // HTTPPrefixPProf is the prefix appended to all Endpoints.
@@ -36,4 +37,26 @@ var Endpoints = map[string]http.Handler{
 	HTTPPrefixPProf + "/threadcreate": pprof.Handler("threadcreate"),
 	HTTPPrefixPProf + "/block":        pprof.Handler("block"),
 	HTTPPrefixPProf + "/mutex":        pprof.Handler("mutex"),
+}
+
+// GetHandlers returns the pprof endpoints for the mgr metrics server.
+//
+// The func can be used in the main.go file of your controller, when initializing the manager:
+//
+//	func main() {
+//		mgrConfig := ctrl.Options{
+//			Metrics: metricsserver.Options{
+//				BindAddress:   metricsAddr,
+//				ExtraHandlers: pprof.GetHandlers(),
+//			},
+//		}
+//	}
+func GetHandlers() map[string]http.Handler {
+	// Only set the fraction if there is no existing setting
+	if runtime.SetMutexProfileFraction(-1) == 0 {
+		// Default to report 1 out of 5 mutex events, on average
+		runtime.SetMutexProfileFraction(5)
+	}
+
+	return Endpoints
 }
