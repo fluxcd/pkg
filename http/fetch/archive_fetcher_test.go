abaaabe0fda32ce906b5f42e4a53118a9cd620e2
Matheus Pimenta <matheuscscp@gmail.com>
2024-02-23 11:07:48 +0000

    Add new FetchWithContext() API to ArchiveFetcher and option for downloading without extracting
    Signed-off-by: Matheus Pimenta <matheuscscp@gmail.com>

--- http/fetch/archive_fetcher_test.go
+++ http/fetch/archive_fetcher_test.go
@@ -17,6 +17,7 @@ limitations under the License.
 package fetch
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -24,8 +25,156 @@ import (
 
 	. "github.com/onsi/gomega"
 
+	"github.com/fluxcd/pkg/tar"
 	"github.com/fluxcd/pkg/testserver"
 )
+
+func TestArchiveFetcher_FetchWithContext(t *testing.T) {
+	g := NewWithT(t)
+	tmpDir := t.TempDir()
+
+	testServer, err := testserver.NewTempArtifactServer()
+	if err != nil {
+		g.Expect(err).NotTo(HaveOccurred(), "failed to create the test server")
+	}
+	fmt.Println("Starting the test server")
+	testServer.Start()
+
+	manifestsFileName := "testdata/manifests.yaml"
+	artifactName := "manifests.tgz"
+	artifactURL := fmt.Sprintf("%s/%s", testServer.URL(), artifactName)
+	artifactChecksum, err := testServer.ArtifactFromDir("testdata", artifactName)
+	g.Expect(err).ToNot(HaveOccurred())
+
+	tests := []struct {
+		name               string
+		url                string
+		digest             string
+		originContentPath  string
+		fetchedContentPath string
+		opts               []Option
+		wantErr            bool
+		wantErrType        error
+	}{
+		{
+			name:               "fetches and verifies the digest",
+			url:                artifactURL,
+			digest:             "sha256:" + artifactChecksum,
+			originContentPath:  manifestsFileName,
+			fetchedContentPath: filepath.Join(tmpDir, manifestsFileName),
+			opts:               []Option{WithUntar()},
+			wantErr:            false,
+		},
+		{
+			name:               "fetches and verifies the digest without extracting",
+			url:                artifactURL,
+			digest:             "sha256:" + artifactChecksum,
+			originContentPath:  filepath.Join(testServer.Root(), artifactName),
+			fetchedContentPath: filepath.Join(tmpDir, artifactName),
+			opts:               []Option{},
+			wantErr:            false,
+		},
+		{
+			name:               "fetches and verifies the digest without extracting, with file name",
+			url:                artifactURL,
+			digest:             "sha256:" + artifactChecksum,
+			originContentPath:  filepath.Join(testServer.Root(), artifactName),
+			fetchedContentPath: filepath.Join(tmpDir, "tarball.tar.gz"),
+			opts:               []Option{WithFileName("tarball.tar.gz")},
+			wantErr:            false,
+		},
+		{
+			name:               "fetches and verifies the checksum",
+			url:                artifactURL,
+			digest:             artifactChecksum,
+			originContentPath:  manifestsFileName,
+			fetchedContentPath: filepath.Join(tmpDir, manifestsFileName),
+			opts:               []Option{WithUntar()},
+			wantErr:            false,
+		},
+		{
+			name:               "breaches max download size",
+			url:                artifactURL,
+			digest:             artifactChecksum,
+			originContentPath:  manifestsFileName,
+			fetchedContentPath: filepath.Join(tmpDir, manifestsFileName),
+			opts:               []Option{WithUntar(), WithMaxDownloadSize(1)},
+			wantErr:            true,
+		},
+		{
+			name:               "breaches max untar size",
+			url:                artifactURL,
+			digest:             artifactChecksum,
+			originContentPath:  manifestsFileName,
+			fetchedContentPath: filepath.Join(tmpDir, manifestsFileName),
+			opts:               []Option{WithUntar(tar.WithMaxUntarSize(1))},
+			wantErr:            true,
+		},
+		{
+			name:               "fails with empty digest error",
+			url:                artifactURL,
+			digest:             "",
+			originContentPath:  manifestsFileName,
+			fetchedContentPath: filepath.Join(tmpDir, manifestsFileName),
+			opts:               []Option{WithUntar()},
+			wantErr:            true,
+		},
+		{
+			name:               "fails with digest parsing error",
+			url:                artifactURL,
+			digest:             "sha1:4c624b40731c876b23d36fe732833ea8261f7f00",
+			originContentPath:  manifestsFileName,
+			fetchedContentPath: filepath.Join(tmpDir, manifestsFileName),
+			opts:               []Option{WithUntar()},
+			wantErr:            true,
+		},
+		{
+			name:               "fails to verify the digest",
+			url:                artifactURL,
+			digest:             "sha256:5c234ee52ff0e3dcc8528d6b9383cc235ad13a11658466f29df3be9eda6ee447",
+			originContentPath:  manifestsFileName,
+			fetchedContentPath: filepath.Join(tmpDir, manifestsFileName),
+			opts:               []Option{WithUntar()},
+			wantErr:            true,
+		},
+		{
+			name:               "fails with not found error",
+			url:                artifactURL + "1",
+			digest:             artifactChecksum,
+			originContentPath:  manifestsFileName,
+			fetchedContentPath: filepath.Join(tmpDir, manifestsFileName),
+			opts:               []Option{WithUntar()},
+			wantErr:            true,
+			wantErrType:        ErrFileNotFound,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			g := NewWithT(t)
+
+			fetcher := New(tt.opts...)
+			err = fetcher.FetchWithContext(context.Background(), tt.url, tt.digest, tmpDir)
+
+			if tt.wantErr {
+				g.Expect(err).To(HaveOccurred())
+				if tt.wantErrType != nil {
+					g.Expect(err).To(Equal(tt.wantErrType))
+				}
+			} else {
+				g.Expect(err).ToNot(HaveOccurred())
+
+				originContent, err := os.ReadFile(tt.originContentPath)
+				g.Expect(err).ToNot(HaveOccurred())
+
+				fetchedContent, err := os.ReadFile(tt.fetchedContentPath)
+				g.Expect(err).ToNot(HaveOccurred())
+
+				g.Expect(string(originContent)).To(BeIdenticalTo(string(fetchedContent)))
+			}
+		})
+	}
+}
 
 func TestArchiveFetcher_Fetch(t *testing.T) {
 	g := NewWithT(t)
@@ -145,5 +294,4 @@ func TestArchiveFetcher_Fetch(t *testing.T) {
 			}
 		})
 	}
-
 }
