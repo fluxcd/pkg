3349c999532c02bd61d1bc8847cb94d31e995d3b
Dipti Pai <diptipai89@outlook.com>
2024-07-26 20:51:25 -0700

    Fix copyright and add comments
    Signed-off-by: Dipti Pai <diptipai89@outlook.com>

--- auth/azure/token_provider.go
+++ auth/azure/token_provider.go
@@ -1,16 +1,17 @@
 /*
 Copyright 2024 The Flux authors
 
-Licensed under the Apache License, Version 2.0 (the "License"); you may not use
-this file except in compliance with the License.
-
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
+
     http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software distributed
-under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-CONDITIONS OF ANY KIND, either express or implied. See the License for the
-specific language governing permissions and limitations under the License.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
 
 package azure
@@ -25,6 +26,11 @@ import (
 )
 
 const (
+	// 499b84ac-1321-427f-aa17-267ca6975798 is the UUID of Azure Devops
+	// resource. The scope defined below provides access to Azure DevOps
+	// Services REST API. The Microsoft Entra ID access token with this scope
+	// can be used to call Azure DevOps API by passing it in the headers as a
+	// Bearer Token : https://learn.microsoft.com/en-us/azure/devops/integrate/get-started/authentication/service-principal-managed-identity?view=azure-devops#q-can-i-use-a-service-principal-or-managed-identity-with-azure-cli
 	AzureDevOpsRestApiScope = "499b84ac-1321-427f-aa17-267ca6975798/.default"
 )
 
