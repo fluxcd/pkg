033a523d381eae84ee1a83c30852a8f351588d42
Sunny <github@darkowlzz.space>
2024-10-17 14:15:43 +0000

    Simplify the cache
    - Remove dependency on kubernetes.
    - Simplify the Store interface to only have Get, Set and Delete.
    - Add new RecordCacheEvent() method in the cache implementations for
      recording the cache events, hit or miss, along with labels of the
      associated flux object being reconciled. The labels name, namespace
      and kind for this metric are not required to be configured when
      creating the cache. They are predefined in the created metrics
      recorder, similar to the runtime metrics recorder for the rest of
      flux. RecordCacheEvent() has to be called by the caller of cache
      operations explicitly along with the label information of the
      associated object being reconciled. The cache no longer has the
      knowledge of the object being reconciled, decoupled.
    - With the decoupled cache and reconciling object for metrics, the
      storeOptions no longer need to have the generic type defined.
      Simplifying the usage.
    - Add new DeleteCacheEvent() method in the cache implementations for
      deleting the cache events, hit or miss, which are associated with the
      object being reconciled. When the reconciling object is deleted, these
      metrics associated with the object must also be deleted.
    - Updated all the tests to use simple string cache.
    - Get() now returns a pointer instead of a separate exists boolean. If
      the pointer is nil, the item is not found in the cache.
    - Get() takes a key and returns a cached item for it. GetByKey() is
      removed as Get() does the same.
    
    Signed-off-by: Sunny <github@darkowlzz.space>

--- cache/store.go
+++ cache/store.go
@@ -17,122 +17,52 @@ limitations under the License.
 package cache
 
 import (
-	"fmt"
 	"time"
 
-	"github.com/fluxcd/cli-utils/pkg/object"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
 // Store is an interface for a cache store.
-// It is a generic version of the Kubernetes client-go cache.Store interface.
-// See https://pkg.go.dev/k8s.io/client-go/tools/cache#Store
-// The implementation should know how to extract a key from an object.
 type Store[T any] interface {
-	// Set adds an object to the store.
-	// It will overwrite the item if it already exists.
-	Set(object T) error
-	// Delete deletes an object from the store.
-	Delete(object T) error
-	// ListKeys returns a list of keys in the store.
-	ListKeys() ([]string, error)
-	// Get returns the object stored in the store.
-	Get(object T) (item T, exists bool, err error)
-	// GetByKey returns the object stored in the store by key.
-	GetByKey(key string) (item T, exists bool, err error)
-	// Resize resizes the store and returns the number of items removed.
-	Resize(int) (int, error)
+	// Set adds an item to the store for the given key.
+	Set(key string, value T) error
+	// Get returns an item stored in the store for the given key.
+	Get(key string) (*T, error)
+	// Delete deletes an item in the store for the given key.
+	Delete(key string) error
 }
 
 // Expirable is an interface for a cache store that supports expiration.
 type Expirable[T any] interface {
 	Store[T]
-	// SetExpiration sets the expiration time for the object.
-	SetExpiration(object T, expiresAt time.Time) error
-	// GetExpiration returns the expiration time for the object in unix time.
-	GetExpiration(object T) (time.Time, error)
-	// HasExpired returns true if the object has expired.
-	HasExpired(object T) (bool, error)
+	// SetExpiration sets the expiration time for a cached item.
+	SetExpiration(key string, expiresAt time.Time) error
+	// GetExpiration returns the expiration time of an item.
+	GetExpiration(key string) (time.Time, error)
+	// HasExpired returns if an item has expired.
+	HasExpired(key string) (bool, error)
 }
 
-type storeOptions[T any] struct {
-	interval    time.Duration
-	registerer  prometheus.Registerer
-	extraLabels []string
-	labelsFunc  GetLvsFunc[T]
+type storeOptions struct {
+	interval   time.Duration
+	registerer prometheus.Registerer
 }
 
 // Options is a function that sets the store options.
-type Options[T any] func(*storeOptions[T]) error
-
-// WithMetricsLabels sets the extra labels for the cache metrics.
-func WithMetricsLabels[T any](labels []string, f GetLvsFunc[T]) Options[T] {
-	return func(o *storeOptions[T]) error {
-		if labels != nil && f == nil {
-			return fmt.Errorf("labelsFunc must be set if labels are provided")
-		}
-		o.extraLabels = labels
-		o.labelsFunc = f
-		return nil
-	}
-}
+type Options func(*storeOptions) error
 
 // WithCleanupInterval sets the interval for the cache cleanup.
-func WithCleanupInterval[T any](interval time.Duration) Options[T] {
-	return func(o *storeOptions[T]) error {
+func WithCleanupInterval(interval time.Duration) Options {
+	return func(o *storeOptions) error {
 		o.interval = interval
 		return nil
 	}
 }
 
 // WithMetricsRegisterer sets the Prometheus registerer for the cache metrics.
-func WithMetricsRegisterer[T any](r prometheus.Registerer) Options[T] {
-	return func(o *storeOptions[T]) error {
+func WithMetricsRegisterer(r prometheus.Registerer) Options {
+	return func(o *storeOptions) error {
 		o.registerer = r
 		return nil
 	}
-}
-
-// KeyFunc knows how to make a key from an object. Implementations should be deterministic.
-type KeyFunc[T any] func(object T) (string, error)
-
-// IdentifiableObject is a wrapper for an object with its identifying metadata.
-type IdentifiableObject struct {
-	object.ObjMetadata
-	// Object is the object that is being stored.
-	Object any
-}
-
-// ExplicitKey can be passed to IdentifiableObjectKeyFunc if you have the key for
-// the objectec but not the object itself.
-type ExplicitKey string
-
-// IdentifiableObjectKeyFunc is a convenient default KeyFunc which knows how to make
-// keys from IdentifiableObject objects.
-func IdentifiableObjectKeyFunc[T any](object T) (string, error) {
-	if key, ok := any(object).(ExplicitKey); ok {
-		return string(key), nil
-	}
-	n, ok := any(object).(IdentifiableObject)
-	if !ok {
-		return "", fmt.Errorf("object has no meta: %v", object)
-	}
-	return n.String(), nil
-}
-
-// StoreObject is a wrapper for an object with its identifying key.
-// It is used to store objects in a Store.
-// This helper is useful when the object does not have metadata to extract the key from.
-// The supplied key can be retrieved with the StoreObjectKeyFunc.
-// When the object has metadata, use IdentifiableObject instead if possible.
-type StoreObject[T any] struct {
-	// Object is the object that is being stored.
-	Object T
-	// Key is the key for the object.
-	Key string
-}
-
-// StoreObjectKeyFunc returns the key for a StoreObject.
-func StoreObjectKeyFunc[T any](object StoreObject[T]) (string, error) {
-	return object.Key, nil
 }
