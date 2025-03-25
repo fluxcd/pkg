221613dbb1e72b935825c09e2cca7c6f58cc3afb
Matheus Pimenta <matheuscscp@gmail.com>
2025-03-25 14:56:32 +0000

    Unvendor controller-runtime lazy rest mapper
    Signed-off-by: Matheus Pimenta <matheuscscp@gmail.com>

--- runtime/client/client.go
+++ runtime/client/client.go
@@ -23,6 +23,7 @@ import (
 	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/client-go/rest"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
 
 	"github.com/fluxcd/cli-utils/pkg/flowcontrol"
 )
@@ -95,7 +96,7 @@ func NewDynamicRESTMapper(restConfig *rest.Config) (meta.RESTMapper, error) {
 		return nil, err
 	}
 
-	restMapper, err := NewLazyRESTMapper(restConfig, httpClient)
+	restMapper, err := apiutil.NewDynamicRESTMapper(restConfig, httpClient)
 	if err != nil {
 		return nil, err
 	}
