29e75f1a3b1fb1dd2c365b82b3572733da2383f5
Matheus Pimenta <matheuscscp@gmail.com>
2026-02-13 13:42:33 +0000

    ssa/jsondiff: expose DiffUnstructured
    Signed-off-by: Matheus Pimenta <matheuscscp@gmail.com>

--- ssa/jsondiff/unstructured_test.go
+++ ssa/jsondiff/unstructured_test.go
@@ -1286,7 +1286,7 @@ func Test_diffUnstructured(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := diffUnstructured(tt.x, tt.y, tt.opts...)
+			got, err := DiffUnstructured(tt.x, tt.y, tt.opts...)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("diffResourceMetadata() error = %v, wantErr %v", err, tt.wantErr)
 				return
