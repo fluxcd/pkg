7497f63a91b42d64775b41de51b427dc2570325b
Adrian Fernandez De La Torre <adri1197@gmail.com>
2026-03-08 14:31:31 +0100

    Migrate to new Kubernentes event API
    Signed-off-by: Adrian Fernandez De La Torre <adri1197@gmail.com>

---
+++ apis/event/v1/metadata.go
@@ -0,0 +1,46 @@
+/*
+Copyright 2026 The Flux authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+// These constants define the Event metadata keys used throughout Flux controllers.
+const (
+	// MetaRevisionKey is the key used to hold the source artifact revision.
+	MetaRevisionKey string = "revision"
+	// MetaOriginRevisionKey is the key used to hold the source artifact origin revision.
+	MetaOriginRevisionKey string = "originRevision"
+	// MetaChecksumKey is the key used to hold the source artifact checksum.
+	// Deprecated: in favor of MetaDigestKey.
+	MetaChecksumKey string = "checksum"
+	// MetaDigestKey is the key used to hold the source artifact digest.
+	MetaDigestKey string = "digest"
+	// MetaTokenKey is the key used to hold an arbitrary token whose contents
+	// are defined on a per-event-emitter basis for uniquely identifying the
+	// contents of the event payload. For example, it could be the generation
+	// of an object, or the hash of a set of configurations, or even a
+	// base64-encoded set of configurations. This is useful for example for
+	// rate limiting the events.
+	MetaTokenKey string = "token"
+	// MetaCommitKey is the key used to hold the Git commit hash.
+	MetaCommitKey string = "commit"
+	// MetaCommitStatusKey is the key used to signal a Git commit status event.
+	MetaCommitStatusKey string = "commit_status"
+	// MetaCommitStatusUpdateValue is the value of MetaCommitStatusKey
+	// used to signal a Git commit status update.
+	MetaCommitStatusUpdateValue string = "update"
+	// MetaChangeRequestKey is the key used to hold the identifier of a change request.
+	MetaChangeRequestKey string = "change_request"
+)
--- runtime/events/recorder_test.go
+++ runtime/events/recorder_test.go
@@ -29,7 +29,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 
-	eventv1 "github.com/fluxcd/pkg/apis/event/v1beta1"
+	eventv1 "github.com/fluxcd/pkg/apis/event/v1"
 )
 
 func TestEventRecorder_AnnotatedEventf(t *testing.T) {
@@ -102,6 +102,7 @@ func TestEventRecorder_AnnotatedEventf(t *testing.T) {
 				require.Equal(t, "webapp", payload.InvolvedObject.Name)
 				require.Equal(t, "gitops-system", payload.InvolvedObject.Namespace)
 				require.Equal(t, "sync", payload.Reason)
+				require.Equal(t, eventv1.ActionReconciling, payload.Action)
 				require.Equal(t, "sync object", payload.Message)
 
 				for k, v := range tt.expectedMetadata {
@@ -117,11 +118,11 @@ func TestEventRecorder_AnnotatedEventf(t *testing.T) {
 
 			const msg = "sync object"
 
-			eventRecorder.AnnotatedEventf(obj, tt.inputAnnotations, corev1.EventTypeNormal, "sync", "%s", msg)
+			eventRecorder.AnnotatedEventf(obj, nil, tt.inputAnnotations, corev1.EventTypeNormal, "sync", eventv1.ActionReconciling, "%s", msg)
 			require.Equal(t, 1, requestCount)
 
 			// When a trace event is sent, it's dropped, no new request.
-			eventRecorder.AnnotatedEventf(obj, tt.inputAnnotations, eventv1.EventTypeTrace, "sync", "%s", msg)
+			eventRecorder.AnnotatedEventf(obj, nil, tt.inputAnnotations, eventv1.EventTypeTrace, "sync", eventv1.ActionReconciling, "%s", msg)
 			require.Equal(t, 1, requestCount)
 		})
 	}
@@ -150,7 +151,7 @@ func TestEventRecorder_AnnotatedEventf_Retry(t *testing.T) {
 	obj.Namespace = "gitops-system"
 	obj.Name = "webapp"
 
-	eventRecorder.AnnotatedEventf(obj, nil, corev1.EventTypeNormal, "sync", "sync %s", obj.Name)
+	eventRecorder.AnnotatedEventf(obj, nil, nil, corev1.EventTypeNormal, "sync", eventv1.ActionReconciling, "sync %s", obj.Name)
 	require.True(t, requestCount > 1)
 }
 
@@ -177,7 +178,7 @@ func TestEventRecorder_AnnotatedEventf_RateLimited(t *testing.T) {
 	obj.Namespace = "gitops-system"
 	obj.Name = "webapp"
 
-	eventRecorder.AnnotatedEventf(obj, nil, corev1.EventTypeNormal, "sync", "sync %s", obj.Name)
+	eventRecorder.AnnotatedEventf(obj, nil, nil, corev1.EventTypeNormal, "sync", eventv1.ActionReconciling, "sync %s", obj.Name)
 	require.Equal(t, 1, requestCount)
 }
 
