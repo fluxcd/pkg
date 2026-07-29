a20ff3a48b4ce40eabc546c443942800acd73ab0
Matheus Pimenta <matheuscscp@gmail.com>
2026-07-29 09:41:09 -0300

    Fix Azure e2e tests after new major version with breaking changes
    Signed-off-by: Matheus Pimenta <matheuscscp@gmail.com>

--- tests/integration/terraform/azure/version.tf
+++ tests/integration/terraform/azure/version.tf
@@ -1,7 +1,11 @@
 terraform {
   required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.20.0, <5.0.0"
+    }
     azuredevops = {
-      source  = "microsoft/azuredevops"
+      source = "microsoft/azuredevops"
     }
   }
 }
